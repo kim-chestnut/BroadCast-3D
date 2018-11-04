466db4f6895e0de37dc2c9917e1508a6f6ade774
strongminmin <15036875971@163.com>
2018-11-04 19:30:28 +0800

    add width

--- BroadCast.js
+++ BroadCast.js
@@ -1,12 +1,13 @@
 ;(function(){
         var root = (typeof self == 'object' && self.self == self && self) || (typeof global == 'object' && global.global == global && global) || this || {};
 
-        function Broadcast(element,arrImg,radius,suspend){
+        function Broadcast(element,arrImg,radius,suspend,width){
             if(this instanceof Broadcast) {
                 this.element = typeof element === 'string' ? document.querySelector(element) : element;
                 this.arrImg =  Object.prototype.toString.call(arrImg).match(/^\[object (\w+)\]$/)[1] == 'Array' ? arrImg : [];
                 this.radius = (typeof radius === "number" && radius) || 250;
                 this.suspend = (typeof suspend === "number" && suspend) || 0;
+                this.width = (typeof width === "number" && width) || 100;
                 this.time = 1000 + (this.suspend *1000);
                 this.len = this.arrImg.length;
                 this.deg = 360 / this.len;
@@ -15,7 +16,7 @@
                 this.init();
                 
             } else {
-                return new Broadcast(element,arrImg,radius,suspend);
+                return new Broadcast(element,arrImg,radius,suspend,width);
             }
         };
 
@@ -31,7 +32,7 @@
             },
             initImgStyle : function(imgArr){
                 for(var i = 0; i < this.len ; i ++) {
-                    imgArr[i].style = "position:absolute;top:0;left:0;right:0;bottom:0;margin: auto;width:100px;";
+                    imgArr[i].style = "position:absolute;top:0;left:0;right:0;bottom:0;margin: auto;width:" + this.width+"px;";
 
                 }
             },  
